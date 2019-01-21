2ddbc8658b0cce72a9fa6b56959be69bb2cc4048
petermcneil <petermcneil@users.noreply.github.com>
2019-01-21 22:13:50 +0000

    - Fix curl

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 brew upgrade cmake
-cd  ../include/catch/ && curl -O https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch2/catch.hpp
+curl -o "$1/include/catch" https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch2/catch.hpp
