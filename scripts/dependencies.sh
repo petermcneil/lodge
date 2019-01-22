0fe29656a9de719ee8c0b838377b8fac7f1ac0b5
petermcneil <petermcneil@users.noreply.github.com>
2019-01-22 12:37:29 +0000

    OS specific configure script

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
-UPGRADE=("cmake")
-INSTALL=("ffmpeg")
+UPGRADE=('cmake')
+INSTALL=('ffmpeg')
+CUR_DIR=$(pwd)
+PARENT_DIR=${CUR_DIR%'/scripts'}
 
 printList(){
     for item in $1 ; do
-        echo ${item}
+        printf '%s\n' ${item}
     done
 }
 
-echo "Updating brew"
-brew update
+if [[ $1 == 'darwin' ]]; then
+    printf '%s\n' '-----------------------------------------------'
+    printf '%s\n' 'OS:           Mac'
+    printf '%s\n' 'Build System: Homebrew'
+    printf '%s\n' '-----------------------------------------------'
+    
+    printf '%s\n' 'Updating brew'
+    brew update
 
-echo "Upgrading the packages:"
-printList ${UPGRADE}
-for item in ${UPGRADE} ; do
-    brew upgrade ${item}
-done
+    printf '%s\n' 'Upgrading the packages:'
+    printList ${UPGRADE}
+    for item in ${UPGRADE} ; do
+        brew upgrade ${item}
+    done
 
-echo "Installing the packages:"
-printList ${INSTALL}
-for item in ${INSTALL} ; do
-    brew install ${item}
-done
+    printf 'Installing the packages:'
+    printList ${INSTALL}
+    for item in ${INSTALL} ; do
+        brew install ${item}
+    done
+fi
 
-echo "Updating catch2"
-curl -o "$1/include/catch/catch.hpp" https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch2/catch.hpp
+printf '%s\n' '-----------------------------------------------'
+printf '%s\n' 'Installing/Updating platform independent code'
+printf '%s\n' '-----------------------------------------------'
+printf '%s\n' 'Updating catch2'
+curl -o '$PARENT_DIR/include/catch/catch.hpp' https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch2/catch.hpp
