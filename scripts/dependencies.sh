8c91b3f00baf3b0a941dcaa3c80e8362ad65f6d9
petermcneil <petermcneil@users.noreply.github.com>
2019-01-22 12:50:24 +0000

    Fix typoe

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -36,4 +36,4 @@ printf '%s\n' '-----------------------------------------------'
 printf '%s\n' 'Installing/Updating platform independent code'
 printf '%s\n' '-----------------------------------------------'
 printf '%s\n' 'Updating catch2'
-curl -o '$PARENT_DIR/include/catch/catch.hpp' https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch2/catch.hpp
+curl -o "$PARENT_DIR/include/catch/catch.hpp" https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch2/catch.hpp
