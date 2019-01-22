d0f37e90671f4e8da0e39eda9a01911a88b6f98b
petermcneil <petermcneil@users.noreply.github.com>
2019-01-22 11:49:37 +0000

    Install ffmpeg with brew on Travis

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+echo "Updating brew"
+brew update
+echo "Upgrading cmake"
 brew upgrade cmake
+echo "Installing ffmpeg"
+brew install ffmpeg
+echo "Updating catch2"
 curl -o "$1/include/catch/catch.hpp" https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch2/catch.hpp
